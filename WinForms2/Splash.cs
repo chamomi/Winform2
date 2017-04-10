fa7af85fc0000ec4d806e443e878d3dfe26c6bd4
chamomi <olgadovbnya97@gmail.com>
2017-04-10 18:47:58 +0200

    Splash screen should be draggable

--- WinForms2/Splash.cs
+++ WinForms2/Splash.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Windows.Forms;
 using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
 
 namespace WinForms2
 {
@@ -18,6 +19,7 @@ namespace WinForms2
         {
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.None;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Splash_MouseDown);
 
             GraphicsPath path = new GraphicsPath();
             path.AddEllipse(new Rectangle(0, 0, 300, 300));
@@ -26,7 +28,23 @@ namespace WinForms2
 
         private void Splash_Load(object sender, EventArgs e)
         {
+        }
 
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Splash_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
         }
 
         ////http://stackoverflow.com/questions/1364115/a-different-requirement-for-a-splash-screen-in-winforms-app  
@@ -47,7 +65,7 @@ namespace WinForms2
                     int fadeStep = (int)Math.Round((double)_fadeTime / 40);
                     _instance.timer1.Interval = fadeStep;
                     double step = 0.05;
-                    for (int ii = 0; ii <= _fadeTime/2; ii += fadeStep)
+                    for (int ii = 0; ii <= _fadeTime / 2; ii += fadeStep)
                     {
                         Thread.Sleep(fadeStep);
                         _instance.Opacity += step;
